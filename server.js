42222a5d3b3300fb476e10acf1b3f80dbf4f287f
Chauncey Kelly <cjkelly221@gmail.com>
2019-10-26 07:23:45 -0400

    deploy ready

--- server.js
+++ server.js
@@ -5,22 +5,34 @@ const movieDB = require('./movies-data-small.json')
 
 const app = express();
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 
-const PORT = 9000;
+app.use(morgan(morganSetting))
+
+const PORT = process.env.PORT ||9000;
 
 app.listen(PORT, () =>{
-  console.log(`Server listening at http://localhost:${PORT}`)
+  
+})
+
+app.use((error, req, res, next)=>{
+  let response
+  if(process.env.NODE_ENV === 'production'){
+    response = {error: { message: 'server error' }}
+  } else {
+    response = {error}
+  }
+  res.status(500).json(response)
 })
 
 //After we set up the server you need to set up the middleware to operate in the order that is required
 //If we want to have an authorization header we create the validation statement before we send the request on to be processed.
 
 app.use(function validateBearerToken(req, res, next){
-  console.log('validate bearer token middleware goes here')
+  
   const apiToken = process.env.API_TOKEN;
   const  authToken = req.get('Authorization');
-  console.log(`This is the token ${process.env.API_TOKEN}`)
+  
   if(!authToken || authToken.split(' ')[1] !==apiToken){
     return res.status(401).json({error: 'Unauthorized request'})
   }
@@ -45,15 +57,11 @@ let handleGetRequests = (req, res) => {
       return res.status(401).json({ error: 'Please provide a number in the request'})
     }
 
-    console.log(num >=Number(rating)) 
     return num >= Number(rating)
   }
 
   //need to set up if statement to accept queries by genre, country, agv vote
   if(req.query.genre){
-    console.log('running genre filter')
-    
-    debugger
     response = response.filter( movies =>{
      return movies.genre.toLowerCase().includes(req.query.genre.toLowerCase())
     })
@@ -61,33 +69,24 @@ let handleGetRequests = (req, res) => {
     
   if(req.query.country){
     response = response.filter( movies => {
-      movies.country.toLowerCase().includes(req.query.country.toLowerCase())
+      return movies.country.toLowerCase().includes(req.query.country.toLowerCase())
     })
   }
 
   if(req.query.rating){
     response = response.filter((movies, index) => {
-      checkRating(movies.avg_vote)
-      
-      
+      return checkRating(movies.avg_vote)
     })
   }
-  console.log(response)
   res.json(response)
 }
 
 //this sets up the express server to respond to the homepage with this request. 
 app.get('/', (req, res)=>{
   res.send("This is the movie DB")
-  console.log('Im getting the request')
   
 })
 
 app.get('/movie', handleGetRequests);
 
 
-
-
-
-
-console.log('hello')
